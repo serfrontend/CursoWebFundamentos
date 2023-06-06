019189230e87770b9617faef00e075f79f2e1586
Daniel Tapias Morales <tapmorales@gmail.com>
2023-06-06 12:08:56 +0100

    first commit

---
+++ ProjetoInfusion/js/main.js
@@ -0,0 +1,26 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
+    
+        
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024 
+    })
+    
+    var carouselImgs = new Carousel({
+        container: '.laptop-slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+    
+    var carouselQuotes = new Carousel({
+        container: '.quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+})()
